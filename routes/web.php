039e8e69e053877299a19a0b1eeb45b175afde97
yoshida-github <yoshidayumajob@gmail.com>
2024-01-19 17:03:14 +0900

    Postコントローラークラスを作成、 Postモデルクラスとマイグレーションファイルを作成、 web.phpにPostControllerのindexメソッドのルーティング定義を追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; //外部にあるPostControllerクラスをインポート
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// /postsへのGETリクエストがあった場合、PostControllerクラスのindexメソッドを呼び出すように設定
+Route::get('/posts', [PostController::class, 'index']);
